8f34abbc110f3c18729df686456e26da265a9dc8
SantiagoAntonelli <santonelliguarro@udesa.edu.ar>
2020-06-17 16:19:16 -0300

    commit 28

--- js/resultados.js
+++ js/resultados.js
@@ -11,32 +11,46 @@ console.log(search);
 var artist = proxy + "https://api.deezer.com/search/artist?q=" + search;
 console.log(artist);
 
-let albums = proxy +"https://api.deezer.com/search/album?q=" + search;
+let albums = proxy + "https://api.deezer.com/search/album?q=" + search;
 
-if(artist.length !=0){
-    fetch(artist)
+let canciones = proxy + "https://api.deezer.com/search/track?q=" + search;
+
+fetch(artist)
     .then(function(response){
         return response.json();
     })
     .then(function(datos){
-        let lista = document.querySelector(".listaresultados");
+        let lista = document.querySelector(".artistas");
         let resultados = datos.data;
         
         resultados.forEach(function(resultado){
             lista.innerHTML += "<li>" + "<a href='details.html' >" + resultado.name + "</a>" + "</li>"
         })
     })
-} else {
-    fetch(albums)
+
+fetch(albums)
     .then(function(respuesta){
         return respuesta.json();
     })
     .then(function(info){
-        let point = document.querySelector(".listaresultados");
+        let point = document.querySelector(".albums");
         let backs = info.data;
         
         backs.forEach(function(back){
-            point.innerHTML += "<li>" + "<a href='details.html' >" + back.title + "</a>" + "</li>"
+            point.innerHTML += "<li>" + "<a href='details.html' >" + back.title + "</a>" + "</li>";
         })
     })
-}
+
+fetch(canciones)
+    .then(function(risposta){
+        return risposta.json();
+    })
+    .then(function(envio){
+        let select = document.querySelector(".canciones");
+        let go = envio.data;
+
+       go.forEach(function(goes){
+           select.innerHTML += "<li>" + "<a href='details.html' >" + goes.title + "</a>" + "</li>";
+       })
+        
+    })
